31ffe4d264f21e5b2318cc285d9c61716204b87c
jango <789456jang@naver.com>
2021-08-21 17:52:56 +0900

    Make Domain and Repository Test

--- src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,10 +31,31 @@ class ItemRepositoryTest {
 
     @Test
     void findAll() {
+        Item itemA = new Item("ItemA", 10000, 10);
+        Item itemB = new Item("ItemB", 20000, 5);
+
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+
+        List<Item>  result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(itemA, itemB);
     }
 
     @Test
     void updateItem() {
-    }
+        Item itemA = new Item("ItemA", 10000, 10);
+        Item saveItem = itemRepository.save(itemA);
+        Long itemId = saveItem.getId();
 
+        Item updateParam = new Item("Item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
 }
